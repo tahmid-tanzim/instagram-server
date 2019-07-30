b328794668ad0709459fec1139ce7a40f61e59a3
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2019-07-30 12:04:20 +0600

    get owners photo

--- index.js
+++ index.js
@@ -38,24 +38,38 @@ app.post('/upload', upload.single('photo'), (req, res) => {
     if (!req.file) {
         res.status(500);
     } else {
+        const uploaded_at = new Date();
+        const {title, owner_id} = req.body;
+        const {filename, size, mimetype} = req.file;
+
         db.run(`INSERT INTO photos (title,filename,size,mimetype,owner_id,uploaded_at) VALUES (?,?,?,?,?,?)`,
-            [
-                req.body.title,
-                req.file.filename,
-                req.file.size,
-                req.file.mimetype,
-                parseInt(req.body.owner_id),
-                new Date()
-            ],
-            (err) => {
+            [title, filename, size, mimetype, parseInt(owner_id), uploaded_at],
+            function (err) {
                 if (err) {
                     console.log(err.message);
                     res.status(400).json({message: err.message});
                 }
-                // get the last insert id
-                console.log(`A row has been inserted with rowid : `, JSON.stringify(this, null, 2));
-                res.status(202).json({message: 'Successfully uploaded photo.'});
+
+                res.status(202).json({
+                    message: 'Successfully uploaded photo.',
+                    data: {id: this.lastID, filename, title, uploaded_at: uploaded_at.getTime()}
+                });
             });
+    }
+});
+
+app.get('/photos', (req, res) => {
+    if (req.query.owner_id) {
+        db.all(`SELECT id, filename, title, uploaded_at FROM photos WHERE owner_id = ? ORDER BY uploaded_at DESC`,
+            [req.query.owner_id],
+            (err, rows) => {
+                if (err) {
+                    console.error(err.message);
+                }
+                res.status(200).json({data: rows});
+            });
+    } else {
+        res.status(400).json({message: 'Invalid Request'});
     }
 });
 
